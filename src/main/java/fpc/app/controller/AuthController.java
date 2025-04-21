9f1f91e10fe6233f527dcd8b1e0ebc093edab8dd
jhonc <calderonjhondairo@gmail.com>
2025-04-21 09:19:47 -0500

    fix: auth header

--- src/main/java/fpc/app/controller/AuthController.java
+++ src/main/java/fpc/app/controller/AuthController.java
@@ -1,7 +1,5 @@
 package fpc.app.controller;
 
-import static fpc.app.constant.Constant.TOKEN_COOKIE_NAME;
-import static fpc.app.constant.Constant.TOKEN_EXPIRATION_TIME;
 
 import fpc.app.dto.app.RegisterUserRequest;
 import fpc.app.dto.auth.LoginRequest;
@@ -9,10 +7,9 @@ import fpc.app.service.auth.impl.AuthenticationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,47 +21,17 @@ public class AuthController {
   private final AuthenticationService authService;
 
   @PostMapping("/login")
-  public ResponseEntity<Void> login(
-      @RequestBody @Valid LoginRequest loginRequest, HttpServletResponse response) {
+  public ResponseEntity<Void> login(@RequestBody @Valid LoginRequest loginRequest) {
     String token = authService.login(loginRequest.getUsername(), loginRequest.getPassword());
-
-    Cookie cookie = new Cookie(TOKEN_COOKIE_NAME, token);
-    cookie.setHttpOnly(true);
-    // cookie.setSecure(true); // Solo en HTTPS (desactivar en desarrollo)
-    cookie.setPath("/");
-    cookie.setMaxAge(TOKEN_EXPIRATION_TIME);
-
-    response.addCookie(cookie);
-    return ResponseEntity.ok().build();
-  }
-
-  @GetMapping("/logout")
-  public ResponseEntity<Void> logout(HttpServletResponse response) {
-    Cookie cookie = new Cookie(TOKEN_COOKIE_NAME, null);
-    cookie.setHttpOnly(true);
-    cookie.setSecure(true);
-    cookie.setPath("/");
-    cookie.setMaxAge(0);
-
-    response.addCookie(cookie);
-    return ResponseEntity.ok().build();
+    return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, token).build();
   }
 
   @Operation(summary = "Register a new user and generate token")
   @ApiResponse(responseCode = "200", description = "User registered successfully")
   @ApiResponse(responseCode = "409", description = "If validation fails")
   @PostMapping("/register")
-  public ResponseEntity<Void> register(
-      @RequestBody @Valid RegisterUserRequest request, HttpServletResponse response) {
+  public ResponseEntity<Void> register(@RequestBody @Valid RegisterUserRequest request) {
     String token = authService.register(request);
-
-    Cookie cookie = new Cookie(TOKEN_COOKIE_NAME, token);
-    cookie.setHttpOnly(true);
-    // cookie.setSecure(true); // Solo en HTTPS (desactivar en desarrollo)
-    cookie.setPath("/");
-    cookie.setMaxAge(TOKEN_EXPIRATION_TIME);
-
-    response.addCookie(cookie);
-    return ResponseEntity.ok().build();
+    return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, token).build();
   }
 }
