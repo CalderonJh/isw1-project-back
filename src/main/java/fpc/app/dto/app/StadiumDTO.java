e949cdce7b5adb3f83ad68cbee5bf850fb19f45d
jhonc <calderonjhondairo@gmail.com>
2025-04-30 10:51:17 -0500

    fix: update to java 21

--- src/main/java/fpc/app/dto/app/StadiumDTO.java
+++ src/main/java/fpc/app/dto/app/StadiumDTO.java
@@ -3,17 +3,7 @@ package fpc.app.dto.app;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.util.List;
-import lombok.*;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class StadiumDTO {
-  @NotBlank
-  @Size(min = 3, max = 250)
-  private String name;
-
-  @Size(min = 1, max = 25)
-  private List<StandDTO> stands;
-}
+public record StadiumDTO(
+    @NotBlank @Size(min = 3, max = 250) String name,
+    @Size(min = 1, max = 25) List<StandDTO> stands) {}
